7bd7dd8a2ab783239e6b7a0ffc7ff252e0a7e292
Jared Mulder <jared-mulder@uiowa.edu>
2024-02-16 08:33:52 -0600

    Continued work on Lab03, added the atsuo file to gitignore

--- Lab03/Lab03/main.asm
+++ Lab03/Lab03/main.asm
@@ -5,8 +5,25 @@
 ; Author : jared
 ;
 
-init:
-	sbi DD
+.include "tn45def.inc"
+.cseg
+.org 0
 
-state1:
+init:
+	; Set up data direction registers
+	sbi DDRB,0
+	sbi DDRB,1
+	sbi DDRB,2
+	cbi DDRB,3
+	sbi DDRB,4
+
+loop:
+	; TODO: Add button debounce code here
+	cpi R16,0
+	
+
+
+rst_counter:
+	
+
 	
