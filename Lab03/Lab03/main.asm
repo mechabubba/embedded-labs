ff32d04cc761cc322e6726939e722445f8c0a1b0
Jared Mulder <jared-mulder@uiowa.edu>
2024-02-16 13:52:56 -0600

    Got the encoder functional
    Don't move the order of anything! the macro is large and causing the
    branch commands to fail when PC offset is too large.

--- Lab03/Lab03/main.asm
+++ Lab03/Lab03/main.asm
@@ -6,6 +6,41 @@
 ;
 
 .include "tn45def.inc"
+
+.macro segwrite
+	cpi @0,0 ; digit=0
+	brne d0
+	ldi @1,0xFC
+d0:	cpi @0,1 ; digit=1
+	brne d1
+	ldi @1,0x60
+d1:	cpi @0,2 ; digit=2
+	brne d2
+	ldi @1,0xDA
+d2:	cpi @0,3 ; digit=3
+	brne d3
+	ldi @1,0xF2
+d3:	cpi @0,4 ; digit=4
+	brne d4
+	ldi @1,0x66
+d4:	cpi @0,5 ; digit=5
+	brne d5
+	ldi @1,0xB6
+d5:	cpi @0,6 ; digit=6
+	brne d6
+	ldi @1,0xBE
+d6:	cpi @0,7 ; digit=7
+	brne d7
+	ldi @1,0xE0
+d7:	cpi @0,8 ; digit=8
+	brne d8
+	ldi @1,0xFE
+d8:	cpi @0,9 ; digit=9
+	brne d9
+	ldi @1,0xF6
+d9:
+.endmacro
+
 .cseg
 .org 0
 
@@ -29,13 +64,7 @@ loop:
 	brie state0 ; If R16 = 0, go to state0.
 	cpi R16,1
 	brie state1 ; If R16 = 1, go to state1.
-
-write_segments: ; Converts the number in R28 into the 7-segment encodings for R29,R30
-	mov R17,R28
-	swap R17
-	andi R17,0x0F ; check the upper nibble of R28 first	 
-
-
+	rjmp write_segments ; Otherwise head straight to the segment encoder
 
 state0:
 	clr R28
@@ -48,5 +77,27 @@ state1:
 	cpi R17,0x0A
 	brlo write_segments ; Goto write_segments if we have not exceeded 9 on the lower digit
 	andi R28,0xF0 ; Clear the lower digit to 0
-	add R28,0x10 ; Add 1 to the upper nibble
+	subi R28,0xF0 ; Add 1 to the upper nibble by subtracting -16, the fact that there is no addi is really stupid
 	rjmp write_segments
+
+write_overflow:
+	ldi R29,0xFD
+	ldi R30,0x8F
+	rjmp write_IO
+
+write_segments: ; Converts the number in R28 into the 7-segment encodings for R29,R30
+	mov R17,R28
+	swap R17
+	andi R17,0x0F ; get the upper nibble of R28 first
+	cpi R17,10
+	breq write_overflow ; If counter is overflowing then write OF instead.
+	segwrite R17,R29 ; Write the left digit
+	sbr R29,0x01 ; Add the decimal point to the left digit
+	mov R17,R28
+	andi R17,0x0F ; Then get the lower nibble of R28
+	segwrite R17,R30 ; Write the right digit
+	rjmp write_IO
+
+write_IO:
+	; SEND TO PINS HERE
+	rjmp loop
