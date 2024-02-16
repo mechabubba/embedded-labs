d410d80b00824a465dd87620b19f31093beaccf7
Jared Mulder <jared-mulder@uiowa.edu>
2024-02-16 10:01:28 -0600

    Wrote the state modes and counter

--- Lab03/Lab03/main.asm
+++ Lab03/Lab03/main.asm
@@ -9,6 +9,12 @@
 .cseg
 .org 0
 
+; R16 will be the state ID register: 0 -> set R28 to 0, 1 -> increment R28, 2 -> hold R28 at value (either pause or overflow)
+; R28 will be the counter, split between the upper and lower nibble. The lower nibble will store the 1s place (0-9),
+; and the upper nibble will store the 10s place (0-10). If the upper nibble reads 10, then overflow has occurred.
+; R29 will be the LED data for the leftmost segment
+; R30 will be the LED data for the rightmost segment
+
 init:
 	; Set up data direction registers
 	sbi DDRB,0
@@ -20,10 +26,27 @@ init:
 loop:
 	; TODO: Add button debounce code here
 	cpi R16,0
-	
+	brie state0 ; If R16 = 0, go to state0.
+	cpi R16,1
+	brie state1 ; If R16 = 1, go to state1.
+
+write_segments: ; Converts the number in R28 into the 7-segment encodings for R29,R30
+	mov R17,R28
+	swap R17
+	andi R17,0x0F ; check the upper nibble of R28 first	 
 
 
-rst_counter:
-	
 
-	
+state0:
+	clr R28
+	rjmp write_segments
+
+state1:
+	inc R28
+	mov R17,R28
+	andi R17,0x0F
+	cpi R17,0x0A
+	brlo write_segments ; Goto write_segments if we have not exceeded 9 on the lower digit
+	andi R28,0xF0 ; Clear the lower digit to 0
+	add R28,0x10 ; Add 1 to the upper nibble
+	rjmp write_segments
