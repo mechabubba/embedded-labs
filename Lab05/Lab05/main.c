f19d5666d50d5ef8e4c672f9ffd7ba9f99654341
Jared Mulder <jared-mulder@uiowa.edu>
2024-03-19 11:05:33 -0500

    Initial Commit

---
+++ Lab05/Lab05/main.c
@@ -0,0 +1,18 @@
+/*
+ * Lab05.c
+ *
+ * Created: 3/19/2024 11:05:17 AM
+ * Author : jared
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
