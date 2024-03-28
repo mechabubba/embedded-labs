04f9ac8244b0825853ecbd9b11ca1e1411ee9927
Jared Mulder <jared-mulder@uiowa.edu>
2024-03-28 12:22:30 -0500

    More clock work, repaired frequency

--- Lab05/Lab05/main.c
+++ Lab05/Lab05/main.c
@@ -8,6 +8,7 @@
 //Note: At 16 MHz, there are 16 clock cycles per microsecond.
 
 #include <avr/io.h>
+#include <avr/power.h>
 #include <util/delay_basic.h>
 #include <avr/sfr_defs.h>
 #include <avr/interrupt.h>
@@ -24,46 +25,60 @@ void lcd_writeByte(const char b);
 void lcd_strobe(void);
 void lcd_initialize(void);
 
-//Global variables:
-uint8_t compare = 0;
+//Global variables (SRAM):
+uint8_t compare = 100;
 
 ISR(TIMER0_OVF_vect) { //Timer0 overflow interrupt.
 	OCR0B = compare; //Assign new PWM compare value.
 }
 
 int main(void) {
-	TCCR0B |= (1 << WGM02);
+	clock_prescale_set(clock_div_2); //Scale down system clock to 8 MHz.
+	
+	TCCR0B |= (1 << WGM02); //Set timer0 to mode 5.
 	TCCR0A &= ~(1 << WGM01);
 	TCCR0A |= (1 << WGM00);
-	lcd_initialize();
 	
+	TCCR0A |= (1 << COM0B1); //Set the OC0B flag to be used for comparing.
+	TCCR0A |= (1 << COM0B0);
+	
+	OCR0A = 200; //Set the TOP value to 200.
+	TIMSK0 |= (1 << TOIE0); //Enable the overflow interrupt for timer0.
+	lcd_initialize(); //Initialize the LCD.
+	
+	sei(); //Enable interrupts.
     while (1) {
 		checkRPG();
+		showDutyCycle();
     }
 }
 
 /* 20 KHz loop with resolution of <=0.75%
  * 1 / 0.0075 = 133.3 -> TOP > 134
- * TOP = 200 -> (16 MHz / 8) / 200 = 10 KHz <- Scale clock timer down by 8.
+ * TOP = 200 -> (16 MHz / 2) / 2*200 = 20 KHz <- Scale system clock down by 2(?).
  * Resolution = 1/200 = 0.5%
  */
 
 void checkRPG(void) {
-	if (bit_is_set(PORTB,1)) compare++;
-	if (bit_is_set(PORTB,0)) compare--;
+	if (bit_is_set(PORTB,PORTB1)) compare++;
+	if (bit_is_set(PORTB,PORTB0)) compare--;
 	if (compare > 200) compare = 200;
 }
 
+void showDutyCycle(void) {
+	lcd_sendCommand(0x01); //Clear the display.
+	lcd_sendCommand(0xC0); //
+}
 
 void lcd_sendText(char text[], uint8_t length) {
-	PORTB |= (1 << 5); //Set RS to data mode.
+	PORTB |= (1 << PORTB5); //Set RS to data mode.
 	for (uint8_t i = 0; i < length; i++) {
 		lcd_writeByte(text[i]); //Send each character in text[] one at a time.
 	}
 }
 
 void lcd_sendCommand(char command) {
-	PORTB &= ~(1 << 5); //Set RS to command mode.
+	PORTB &= ~(1 << PORTB5); //Set RS to command mode.
 	lcd_writeByte(command);
 	if (command == 0x01 || command == 0x02 || command == 0x03) { //If command is CLEAR_DISPLAY or CURSOR_HOME, then:
 		_delay_loop_2(6800); //Wait 6800 iterations = 27200 clock cycles = 1700us.
