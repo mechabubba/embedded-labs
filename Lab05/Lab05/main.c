5b52d01bba8b6cead3a97b66d547b8f5f2fcafa9
mechabubba <stevenvanniisprettycool@gmail.com>
2024-03-29 11:57:06 -0500

    Revert "replace hand ported lcd code w/ library"
    This reverts commit ac8d561f58202d17f93f166bc9db9749d2d702f7.

--- Lab05/Lab05/main.c
+++ Lab05/Lab05/main.c
@@ -12,12 +12,6 @@
 #include <util/delay_basic.h>
 #include <avr/sfr_defs.h>
 #include <avr/interrupt.h>
-#include "lcd.h"
-
-#define LCD_RS_PORT PORTB
-#define LCD_RS_PIN PINB5
-#define LCD_E_PORT PORTB
-#define LCD_E_PIN PINB3
 
 #ifdef F_CPU
 #define F_CPU 16000000UL //16 MHz clock speed assigned for the ATmega328P.
@@ -26,11 +20,11 @@
 //Function Prototypes:
 void checkRPG(void);
 void showDutyCycle(void);
-//void lcd_sendText(char text[], uint8_t length);
-//void lcd_sendCommand(char command);
-//void lcd_writeByte(const char b);
-//void lcd_strobe(void);
-//void lcd_initialize(void);
+void lcd_sendText(char text[], uint8_t length);
+void lcd_sendCommand(char command);
+void lcd_writeByte(const char b);
+void lcd_strobe(void);
+void lcd_initialize(void);
 
 //Global variables (SRAM):
 uint8_t compare = 50;
@@ -58,8 +52,7 @@ int main(void) {
 	OCR0B = 50u; //Set initial COMPARE value to 100.
 	TCCR0B |= (1 << CS00); //Turn on the clock with no pre-scaling.
 	
-	//lcd_initialize(); //Initialize the LCD.
-	lcd_init(LCD_DISP_ON);
+	lcd_initialize(); //Initialize the LCD.
 	
 	sei(); //Enable interrupts.
     while (1) {
@@ -82,9 +75,66 @@ void checkRPG(void) {
 }
 
 void showDutyCycle(void) {
-	//lcd_sendCommand(0x01); //Clear the display.
-	//lcd_sendCommand(0xC0); //Move text pointer to 40
-	lcd_clrscr();
-	lcd_home();
-	lcd_puts_p("Duty cycle = smth");
+	lcd_sendCommand(0x01); //Clear the display.
+	
+	lcd_sendCommand(0xC0); //Move text pointer to 40
+	lcd_sendText("Duty cycle = ",13);
 }
+
+void lcd_sendText(char text[], uint8_t length) {
+	PORTB |= (1 << PORTB5); //Set RS to data mode.
+	for (uint8_t i = 0; i < length; i++) {
+		lcd_writeByte(text[i]); //Send each character in text[] one at a time.
+	}
+}
+
+void lcd_sendCommand(char command) {
+	PORTB &= ~(1 << PORTB5); //Set RS to command mode.
+	lcd_writeByte(command);
+	if (command == 0x01 || command == 0x02 || command == 0x03) { //If command is CLEAR_DISPLAY or CURSOR_HOME, then:
+		_delay_loop_2(6800); //Wait 6800 iterations = 27200 clock cycles = 1700us.
+	}
+}
+
+void lcd_writeByte(const char b) {
+	char tmp = b & 0xF0; //Set upper nibble of tmp to upper nibble of b.
+	tmp >>= 4; //Swap upper and lower nibble of tmp. I don't know if we have access to a method like SWAP, so this might be slow.
+	char pcUpper = PORTC & 0xF0; //Store the upper nibble of PORTC.
+	PORTC = pcUpper | tmp; //Write upper nibble of b to the output.
+	lcd_strobe(); //Strobe the enable line, then run the 0.5us delay.
+	_delay_loop_1(3); //3 iterations >= 9 clock cycles > 0.5us.
+	
+	tmp = b * 0x0F; //Set lower nibble of tmp to lower nibble of b.
+	pcUpper = PORTC & 0xF0; //Store the upper nibble of PORTC again.
+	PORTC = pcUpper | tmp; //Write lower bits of b to the output.
+	lcd_strobe();
+	_delay_loop_1(214); //214 iterations >= 642 clock cycles > 40us (For standard commands and text entry).
+}
+
+void lcd_strobe(void) {
+	PORTB &= ~(1 << 3); //Clear PB3.
+	PORTB |= (1 << 3); //Set PB3 (Enable).
+	_delay_loop_1(2); //2 iteration delay loop guarantees at least 6 clock cycles pass, which is over 230ns.
+	PORTB &= ~(1 << 3); //Clear PB3 (Enable).
+}
+
+void lcd_initialize(void) {
+	for (int i = 0; i < 6; i++) {
+		_delay_loop_2(0);
+	}
+	_delay_loop_2(6784); //Should total to approximately 0.1 seconds
+	
+	lcd_sendCommand(0x03); //Set LCD to 8-bit mode.
+	_delay_loop_2(20000); //Wait 5ms.
+	lcd_sendCommand(0x03); //Set LCD to 8-bit mode.
+	_delay_loop_2(800); //Wait 200us.
+	lcd_sendCommand(0x03); //Set LCD to 8-bit mode.
+	_delay_loop_2(800); //Wait 200us.
+	lcd_sendCommand(0x02); //Set LCD to 4-bit mode.
+	_delay_loop_2(800); //Wait 200us.
+	
+	lcd_sendCommand(0x28); //Set LCD to 4-bit, 2-line, 5x7-display mode.
+	lcd_sendCommand(0x06); //Set LCD to increment, display shift off.
+	lcd_sendCommand(0x0C); //Set LCD display on.
+}
+
