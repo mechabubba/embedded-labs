6fbc793d4dfadc9d6016e77533a254ed6f64ec96
Jared Mulder <jared-mulder@uiowa.edu>
2024-04-02 12:31:13 -0500

    Added some state control code

--- Lab05/Lab05/main.c
+++ Lab05/Lab05/main.c
@@ -28,9 +28,10 @@ const char warnText[] PROGMEM = "Low RPM";
 const char stallText[] PROGMEM = "Fan Stalled";
 
 //Global variables (SRAM):
-uint8_t compare = 50; //Compare value, complement to the PWM percentage.
+uint8_t compare = 50; //Compare value, equal to PWM percentage.
 uint16_t tachometer = 0;
-char* poop;
+bool displayState = 0; //State value (0-1), state 0 is status mode and state 1 is duty cycle mode (for LCD).
+char* buffer;
 
 ISR(INT1_vect) { //External Interrupt 1 (Tachometer).
 	TCCR1B &= ~0x03; //Turn off the clock.
@@ -44,6 +45,7 @@ ISR(INT1_vect) { //External Interrupt 1 (Tachometer).
 //This equates to 1 to 100 Hz (Revolutions per second). At 8 MHz, this means 4 million half-cycles can occur in the worst case.
 //If we divide Timer1 by 64, that gets us to a worst case of 62500 half-cycles, which can just barely fit within the 16-bit counter space of 65535.
 //We can then assume that if the 16-bit counter hits its max value, a stall condition has been met.  
+//The value out of the 16-bit timer (global variable tachometer) is thus in units of 8us / half-cycle.
 
 ISR(TIMER0_OVF_vect) { //Timer0 overflow interrupt.
 	OCR0B = compare; //Assign new PWM compare value.
@@ -63,7 +65,7 @@ int main(void) {
 	TCCR0A &= ~(1 << WGM01);
 	TCCR0A |= (1 << WGM00);
 	TCCR0A |= (1 << COM0B1); //Set the OC0B flag to be used for comparing.
-	TCCR0A |= (1 << COM0B0);
+	TCCR0A &= ~(1 << COM0B0);
 	TIMSK0 |= (1 << TOIE0); //Enable the overflow interrupt for timer0.
 	OCR0A = 200u; //Set the TOP value to 200.
 	OCR0B = 50u; //Set initial COMPARE value to 100.
@@ -73,9 +75,12 @@ int main(void) {
 	lcd_init(LCD_DISP_ON); //Initialize the LCD.
 	lcd_clrscr();
 	
+	uint8_t refresh = 0;
 	sei(); //Enable interrupts.
     while (1) {
 		checkRPG();
+		if (refresh == 0) updateLCD(); //LCD is only updated every 256 cycles to increase visibility.
+		refresh++;
     }
 }
 
@@ -93,6 +98,14 @@ void checkRPG(void) {
 }
 
 void updateLCD(void) {
+	if (bit_is_clear(PORTD,PORTD2)) displayState = displayState ? 0 : 1; //Ternary operator to toggle the state variable.
 	lcd_clrscr();
-	lcd_puts(itoa(compare, poop, 10));
+	lcd_home();
+	lcd_puts(itoa(compare, buffer, 10)); //Display RPM.
+	lcd_gotoxy(0,1);
+	if (displayState) {
+		lcd_puts(itoa(compare, buffer, 10)); //State 1 - display duty cycle.
+	} else {
+		//State 0 - display fan status.
+	}
 }
