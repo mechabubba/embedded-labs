f0de6fe19674021bfb6bfd6fdebc59dc0fc76647
mechabubba <stevenvanniisprettycool@gmail.com>
2024-04-23 14:06:41 -0500

    init term project

---
+++ TermProject/TermProject/main.c
@@ -0,0 +1,18 @@
+/*
+ * TermProject
+ *
+ * Created: 4/23/2024 1:58:54 PM
+ * Author : Jared Mulder, Steven Vanni
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
